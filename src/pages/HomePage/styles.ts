88341d087d70a66a96df5791ddd8c7d1d257b951
Lara <laragalvanim@gmail.com>
2022-03-29 22:09:35 +0000

    Adjust Home page so the layout can be mobile friendly

--- src/pages/HomePage/styles.ts
+++ src/pages/HomePage/styles.ts
@@ -5,6 +5,18 @@ export const StyledTitle = styled("div")(({ theme }) => ({
     fontSize: "4rem",
     lineHeight: "5.5rem",
     color: theme.palette.text.disabled,
+    "@media (max-width: 1280px)": {
+        fontSize: "3rem",
+        lineHeight: "4rem",
+    },
+    "@media (max-width: 1080px)": {
+        fontSize: "2rem",
+        lineHeight: "3rem",
+    },
+    "@media (max-width: 400px)": {
+        fontSize: "1.8rem",
+        lineHeight: "3rem",
+    },
 }));
 
 export const StyledSubtitle = styled("div")(({ theme }) => ({
@@ -12,4 +24,9 @@ export const StyledSubtitle = styled("div")(({ theme }) => ({
     fontSize: "1rem",
     lineHeight: "2rem",
     color: theme.palette.text.disabled,
+    textAlign: "justify",
+    textJustify: "inter-word",
+    "@media (max-width: 1080px)": {
+        fontSize: "0.95rem",
+    },
 }));
--- src/styles.ts
+++ src/styles.ts
@@ -1,17 +1,13 @@
 import { styled } from "@mui/material/styles";
 
 export const MainStyle = styled("div")(({ theme }) => ({
-    height: "100%",
-    width: "100%",
-    marginTop: "5%",
-    boxSizing: "border-box",
-    "@media (max-width: 1280px)": {
+    height: "100vh",
+    width: "100vw",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    "@media (max-width: 959px)": {
         marginTop: "10%",
-    },
-    "@media (max-width: 800px)": {
-        marginTop: "15%",
-    },
-    "@media (max-width: 600px)": {
-        marginTop: "20%",
+        height: "90vh",
     },
 }));
