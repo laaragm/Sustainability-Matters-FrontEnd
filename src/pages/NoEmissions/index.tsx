06bc9a712f34c9faeb050732d139bfba3c043fa1
MVAPereira <mvapereira97@gmail.com>
2022-04-03 13:02:27 +0100

    Start implementing no emissions page

---
+++ src/pages/NoEmissions/index.tsx
@@ -0,0 +1,31 @@
+import { Stack, useTheme } from '@mui/material';
+import { StyledSubtitle, StyledTitle } from './styles';
+
+export default function NoEmissions() {
+    const theme = useTheme();
+
+    const content = (
+        <>
+            <StyledTitle>Hi there 👋</StyledTitle>
+
+            <StyledSubtitle>
+                Thank you for your registration!
+                Now you can start tracking your CO2 emissions.
+            </StyledSubtitle>    
+        </> 
+    );
+
+    return (
+        <>
+            <Stack>
+                {content} 
+            </Stack>
+        </>
+    )
+
+
+
+
+
+
+}
---
+++ src/pages/NoEmissions/styles.ts
@@ -0,0 +1,19 @@
+
+import { styled } from '@mui/material/styles';
+
+
+export const StyledTitle = styled("div")(({ theme }) => ({
+    fontWeight: 700,
+    fontSize: "4rem",
+    lineHeight: "5.5rem",
+    color: theme.palette.text.secondary,
+}));
+
+export const StyledSubtitle = styled("div")(({ theme })=> ({
+    fontWeight: 600,
+    fontSize: "1rem",
+    lineHeight: "2rem",
+    color: theme.palette.text.secondary,
+    textAlign: "justify",
+    textJustify: "inter-word",
+}));
