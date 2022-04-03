7aa96b72ed71213b13d690cde996612e05d5298c
MVAPereira <mvapereira97@gmail.com>
2022-04-03 22:40:51 +0100

    Implemented textfields and register button

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,46 +1,103 @@
 import { Box, Stack, useTheme } from '@mui/material';
 import { useState } from 'react';
+import { CustomizedButton } from '../../shared/components/CustomizedButton';
 import { CustomizedTextField } from '../../shared/components/CustomizedTextField';
+import { StyledTitle } from './styles';
 
 
 export default function SignUp() {
 
     const theme = useTheme();
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastname] = useState("");
     const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
+    const handleRegister: () => void = () => 1
+    
+    const handleFirstName = (value: string) => {
+        setFirstName(value);
+    };
+    const handleLastName = (value: string) => {
+        setLastname(value);
+    };
     const handleEmail = (value: string) => {
         setEmail(value);
     };
-
-
+    const handlePassword = (value: string) => {
+        setPassword(value);
+    };
 
     return (
         <>
             <Stack
+                alignItems="center"
                 sx={{
                     width:600,
                     height:630,
-                    backgroundColor:'#ffffff',
+                    backgroundColor:'white',
                     borderRadius:"1.5rem",
                 }}
             >
-            <Stack>
-                <CustomizedTextField
+                <Stack
+                alignItems="baseline"
+                width="90%"
+                >
+                    <StyledTitle>
+                        CREAT YOUR ACCOUNT
+                    </StyledTitle>
+                </Stack>
+                <Stack width="70%">
+                    <CustomizedTextField
+                        title="First Name"
+                        id="first-name-field"
+                        type="text"
+                        value={firstName}
+                        onChange={(value) => handleFirstName(value)}
+                    />
+                </Stack>
 
-                    title="Email"
-                    id="email-field-login"
-                    type="email"
-                    value={email}
-                    onChange={(value) => handleEmail(value)}
-                />
+                <Stack width="70%">
+                    <CustomizedTextField
+                        title="Last Name"
+                        id="last-name-field"
+                        type="text"
+                        value={lastName}
+                        onChange={(value) => handleLastName(value)}
+                    />
+                </Stack>                
 
-            </Stack>    
+                <Stack width="70%">
+                    <CustomizedTextField
+                        title="Email"
+                        id="email-field"
+                        type="email"
+                        value={email}
+                        onChange={(value) => handleEmail(value)}
+                    />
+                </Stack>
 
+                <Stack width="70%">
+                    <CustomizedTextField
+                        title="Password"
+                        id="password-field-login"
+                        type="password"
+                        value={password}
+                        onChange={(value) => handlePassword(value)}
+                    />
+                </Stack>
 
-
+                <Stack width="70%">
+                    <CustomizedButton
+                        color="secondary"
+                        borderRadius="1.5rem"
+                        fullWidth={true}
+                        onClick={handleRegister}
+                    >
+                        Register
+                    </CustomizedButton>
+                </Stack>
             </Stack>
-
-
         </>
     );
 }
