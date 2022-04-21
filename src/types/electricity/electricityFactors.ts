05fbe4eb7f4a0a71bf72de92dbc2a0728c165bb8
Lara <laragalvanim@gmail.com>
2022-04-21 11:07:02 +0000

    Adjust electricity types

---
+++ src/types/electricity/electricityFactors.ts
@@ -0,0 +1,87 @@
+const europe = 0.276;
+const usa = 0.493;
+const china = 0.681;
+const world = 0.519;
+const russia = (0.373 + 0.465) / 2;
+const india = (0.643 + 0.473 + 0.097) / 3;
+const israel = 0.58;
+const turkey = 0.394;
+const nigeria = 0.49;
+const canada = (0.04 + 0.506 + 0.02 + 0.03 + 0.509) / 5;
+const iceland = 0.028;
+const france = 0.035;
+const uk = 0.313;
+const ireland = 0.472;
+const spain = 0.138;
+const portugal = 0.296;
+const netherlands = 0.466;
+const belgium = 0.09;
+const denmark = 0.192;
+const germany = 0.304;
+const poland = 0.725;
+const italy = (0.249 + 0.237 + 0.452 + 0.449 + 0.354 + 0.52) / 6;
+const finland = 0.094;
+const norway = (0.026 + 0.031 + 0.039 + 0.029 + 0.033) / 5;
+const czechia = 0.406;
+const austria = 0.096;
+const serbia = 0.548;
+const slovenia = 0.278;
+const hungary = 0.217;
+const slovakia = 0.226;
+const ukraine = 0.242;
+const romania = 0.275;
+const moldava = 0.667;
+const estonia = 0.46;
+const latvia = 0.44;
+const lithuania = 0.361;
+const peru = 0.026;
+const bolivia = 0.377;
+const argentina = 0.33;
+const uruguay = 0.33;
+const brazil = (0.282 + 0.196 + 0.261 + 0.276) / 4;
+const australia = (0.708 + 0.303 + 0.641 + 0.455 + 0.22) / 5;
+
+export const electricityFactors = {
+    europe: europe,
+    usa: usa,
+    china: china,
+    world: world,
+    france: france,
+    uk: uk,
+    ireland: ireland,
+    spain: spain,
+    portugal: portugal,
+    netherlands: netherlands,
+    belgium: belgium,
+    denmark: denmark,
+    germany: germany,
+    poland: poland,
+    italy: italy,
+    turkey: turkey,
+    finland: finland,
+    norway: norway,
+    czechia: czechia,
+    austria: austria,
+    serbia: serbia,
+    slovenia: slovenia,
+    hungary: hungary,
+    slovakia: slovakia,
+    ukraine: ukraine,
+    romania: romania,
+    moldava: moldava,
+    estonia: estonia,
+    latvia: latvia,
+    lithuania: lithuania,
+    peru: peru,
+    bolivia: bolivia,
+    argentina: argentina,
+    uruguay: uruguay,
+    brazil: brazil,
+    australia: australia,
+    russia: russia,
+    india: india,
+    nigeria: nigeria,
+    canada: canada,
+    iceland: iceland,
+    israel: israel,
+};
