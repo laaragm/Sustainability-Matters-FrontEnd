4ca927bdbd3d0f40e6d39c1ade9c0494a0b362fb
Lara <laragalvanim@gmail.com>
2022-04-24 13:37:15 +0000

    Implement EmissionInformation component

---
+++ src/pages/Emission/components/EmissionInformation/styles.ts
@@ -0,0 +1,21 @@
+import { styled } from "@mui/material/styles";
+
+export const StyledLabel = styled("div")(({ theme }) => ({
+    color: theme.palette.text.secondary,
+    fontSize: "1rem",
+    lineHeight: "1.25rem",
+    fontWeight: 700,
+    "@media (max-width: 400px)": {
+        fontSize: "0.9rem",
+    },
+}));
+
+export const StyledValue = styled("div")(({ theme }) => ({
+    color: theme.palette.warning.main,
+    fontSize: "1rem",
+    lineHeight: "1.25rem",
+    fontWeight: 400,
+    "@media (max-width: 400px)": {
+        fontSize: "0.9rem",
+    },
+}));
