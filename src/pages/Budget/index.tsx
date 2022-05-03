5cd6c94a8662b6a5c7ca6173c8a899522f775f6b
MVAPereira <mvapereira97@gmail.com>
2022-05-03 11:58:31 +0100

    ajusting modal size

--- src/pages/Budget/index.tsx
+++ src/pages/Budget/index.tsx
@@ -1,8 +1,11 @@
-import { Stack } from '@mui/material';
+import { Stack, useMediaQuery, useTheme } from '@mui/material';
 import budgetIllustration from "../../assets/images/budgetPageIllustration.svg";
 import { StyledTitle, StyledText } from './styles';
 
 export default function Budget() {
+
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
     return (
         <>
@@ -16,6 +19,22 @@ export default function Budget() {
                         height="90%"
                         width= "90%"
                     />
+            </Stack>
+            <Stack
+                alignItems="center"
+                direction="column"
+                justifyContent="space-evenly"
+                spacing={isMobile ? 1 : 0.1}
+                sx={{
+                    width:"34.0rem",
+                    height:"38.0rem",
+                    backgroundColor:'white',
+                    borderRadius:"1.5rem",
+                }}
+            >
+                <StyledTitle>
+                    C02 consumption based on European standards
+                </StyledTitle>
             </Stack>
         </>
     )
