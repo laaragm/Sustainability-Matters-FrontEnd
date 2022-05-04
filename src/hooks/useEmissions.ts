f3e23c89ed023fc66a62c5834fbfa9d6071d4d88
Lara <laragalvanim@gmail.com>
2022-05-04 17:34:24 +0000

    Adjust useEmissions hook

--- src/hooks/useEmissions.ts
+++ src/hooks/useEmissions.ts
@@ -8,44 +8,19 @@ type GetEmissionsResponse = {
     emissions: { [key: string]: Emission[] };
 };
 
-export async function getEmissions(
-    page: number
-): Promise<GetEmissionsResponse> {
-    const { data, headers } = await api.get("emissions", {
-        params: {
-            page,
-        },
-    });
-    const totalCount = +headers["x-total-count"];
-    const result: Emission[] = data.emissions?.map((item: Emission) => {
-        return {
-            title: item.title,
-            subcategory: item.subcategory,
-            amount: item.amount,
-            co2Emission: +(
-                item.amount * item.subcategory?.emissionFactor
-            ).toFixed(3),
-            user: item.user,
-            date: new Date(item.date).toLocaleDateString("en", {
-                day: "2-digit",
-                month: "long",
-                year: "numeric",
-            }),
-        };
-    });
-    const emissions = {
-        "April 2022": result.slice(0, 3),
-        "March 2022": result.slice(3, 6),
-        "February 2022": result.slice(6, 9),
-    };
+export async function getEmissions(): Promise<GetEmissionsResponse> {
+    const response = await api.get("monthemission/month");
+    const emissions = response?.data;
+    console.log("RESPONSE: ", emissions);
+    const totalCount = emissions?.length;
 
     return { emissions, totalCount };
 }
 
-export function useEmissions(page: number, options?: UseQueryOptions) {
+export function useEmissions(options?: UseQueryOptions) {
     const { data, isLoading, isFetching, error } = useQuery(
-        ["emissions", page],
-        () => getEmissions(page),
+        ["emissions"],
+        () => getEmissions(),
         // @ts-ignore
         {
             staleTime: 1000 * 15,
