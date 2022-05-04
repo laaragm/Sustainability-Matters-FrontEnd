604467e05eba3056e80d6ea2f1e9f5bfdd53befb
Lara <laragalvanim@gmail.com>
2022-05-04 17:34:49 +0000

    Adjust api

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -2,9 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
     baseURL: process.env.REACT_APP_BASE_REQUEST_URL,
-    // TODO: Change this
     headers: {
-        Authorization: `Bearer b'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwdWJsaWNfaWQiOiJzdHJpbmciLCJleHAiOjE2NTE2NzQxNTZ9.e0_ueK2C2T5x5VL38MTZe4YNS0AV7375N_awyNhuERk'
-    `,
+        Authorization: `Bearer ${localStorage.getItem("REACT_TOKEN_AUTH")}`,
     },
 });
