eff957b5df4a11bd4b0748f42394a950605d0b77
Lara <laragalvanim@gmail.com>
2022-06-14 16:43:19 +0000

    Adjust responsivity of the Budget page

--- src/pages/Budget/styles.ts
+++ src/pages/Budget/styles.ts
@@ -1,27 +1,46 @@
-import { styled } from '@mui/material/styles';
+import { styled } from "@mui/material/styles";
 
+interface StyledCardProps {
+    isMobile: boolean;
+}
 
 export const StyledTitle = styled("div")(({ theme }) => ({
     fontWeight: 750,
     fontSize: "1.15rem",
-    lineHeight: "5.5rem",
+    lineHeight: "2.5rem",
     color: theme.palette.text.secondary,
+    textAlign: "center",
 }));
 
-export const StyledSubtitle = styled("div")(({ theme })=> ({
-    fontWeight: 550,
-    fontSize: "1rem",
-    lineHeight: "2rem",
-    color: theme.palette.text.secondary,
-    textAlign: "left",
-    textJustify: "inter-word",
-}));
+export const StyledSubtitle = styled("div")<StyledCardProps>(
+    ({ theme, isMobile }) => ({
+        fontWeight: 550,
+        fontSize: isMobile ? "0.9rem" : "1rem",
+        lineHeight: "2rem",
+        color: theme.palette.text.secondary,
+        textAlign: "left",
+        textJustify: "inter-word",
+    })
+);
 
-export const StyledText = styled("div")(({ theme })=> ({
-    fontWeight: 400,
-    fontSize: "1rem",
-    lineHeight: "2rem",
-    color: theme.palette.grey[500],
-    textAlign: "left",
-    textJustify: "inter-word",
-}));
+export const StyledText = styled("div")<StyledCardProps>(
+    ({ theme, isMobile }) => ({
+        fontWeight: 400,
+        fontSize: isMobile ? "0.9rem" : "1rem",
+        lineHeight: "2rem",
+        color: theme.palette.grey[500],
+        textAlign: "right",
+        textJustify: "inter-word",
+    })
+);
+
+export const StyledCard = styled("div")<StyledCardProps>(
+    ({ theme, isMobile }) => ({
+        backgroundColor: theme.palette.common.white,
+        color: theme.palette.common.black,
+        borderRadius: "1.25rem",
+        padding: isMobile ? "1rem" : "3rem",
+        width: isMobile ? "90%" : "40%",
+        height: isMobile ? "90%" : "60%",
+    })
+);
