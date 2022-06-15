7e6fb83b27dffa9d56955d3fb139de433ed47b1a
Lara <laragalvanim@gmail.com>
2022-06-15 11:06:09 +0000

    Add loading to Budget page

--- src/pages/Budget/index.tsx
+++ src/pages/Budget/index.tsx
@@ -1,10 +1,15 @@
-import { Box, Stack, useMediaQuery, useTheme } from "@mui/material";
+import {
+    CircularProgress,
+    Stack,
+    useMediaQuery,
+    useTheme,
+} from "@mui/material";
+
 import budgetIllustration from "../../assets/images/budgetPageIllustration.svg";
-import { StyledTitle, StyledSubtitle, StyledText, StyledCard } from "./styles";
 import { CustomizedBox } from "../../shared/components/CustomizedBox";
 import { CustomizedMessage } from "../../shared/components/CustomizedMessage";
-
 import { useBudget } from "../../hooks/useBudget";
+import { StyledTitle, StyledSubtitle, StyledText, StyledCard } from "./styles";
 
 export default function Budget() {
     const theme = useTheme();
@@ -25,104 +30,119 @@ export default function Budget() {
                 />
             </Stack>
             <StyledCard isMobile={isMobile}>
-                <Stack
-                    alignItems="center"
-                    direction="column"
-                    justifyContent={isMobile ? "flex-start" : "space-evenly"}
-                    spacing={isMobile ? 2 : 5}
-                >
-                    <StyledTitle>
-                        C02 consumption based on European standards
-                    </StyledTitle>
-                    <Stack mt={3}>
-                        <Stack mb={2}>
-                            <StyledSubtitle isMobile={isMobile}>
-                                Daily
-                            </StyledSubtitle>
-                            <Stack
-                                direction="row"
-                                justifyContent="space-between"
-                            >
-                                <CustomizedBox
+                {isLoading ? (
+                    <Stack
+                        width="100%"
+                        height="100%"
+                        alignItems="center"
+                        justifyContent="center"
+                    >
+                        <CircularProgress color="inherit" />
+                    </Stack>
+                ) : (
+                    <Stack
+                        alignItems="center"
+                        direction="column"
+                        justifyContent={
+                            isMobile ? "flex-start" : "space-evenly"
+                        }
+                        spacing={isMobile ? 2 : 5}
+                    >
+                        <StyledTitle>
+                            C02 consumption based on European standards
+                        </StyledTitle>
+                        <Stack mt={3}>
+                            <Stack mb={2}>
+                                <StyledSubtitle isMobile={isMobile}>
+                                    Daily
+                                </StyledSubtitle>
+                                <Stack
+                                    direction="row"
+                                    justifyContent="space-between"
+                                >
+                                    <CustomizedBox
+                                        // @ts-ignore
+                                        consumption={posts?.budget.day}
+                                        type="day"
+                                    />
+                                    <StyledText isMobile={isMobile}>
+                                        {/* @ts-ignore */}
+                                        {posts?.budget.day > 0
+                                            ? // @ts-ignore
+                                              posts?.budget.day?.toFixed(3)
+                                            : posts?.budget.day}{" "}
+                                        kgCO2eq{" "}
+                                    </StyledText>
+                                </Stack>
+                                <CustomizedMessage
                                     // @ts-ignore
                                     consumption={posts?.budget.day}
                                     type="day"
                                 />
-                                <StyledText isMobile={isMobile}>
-                                    {/* @ts-ignore */}
-                                    {posts?.budget.day > 0
-                                        ? // @ts-ignore
-                                          posts?.budget.day?.toFixed(3)
-                                        : posts?.budget.day}{" "}
-                                    kgCO2eq{" "}
-                                </StyledText>
                             </Stack>
-                            <CustomizedMessage
-                                // @ts-ignore
-                                consumption={posts?.budget.day}
-                                type="day"
-                            />
-                        </Stack>
 
-                        <Stack mb={2}>
-                            <StyledSubtitle isMobile={isMobile}>
-                                Monthly
-                            </StyledSubtitle>
-                            <Stack
-                                direction="row"
-                                justifyContent="space-between"
-                            >
-                                <CustomizedBox
-                                    consumption={
-                                        // @ts-ignore
-                                        posts?.budget.month > 0
-                                            ? // @ts-ignore
-                                              posts?.budget.month?.toFixed(3)
-                                            : posts?.budget.month
-                                    }
+                            <Stack mb={2}>
+                                <StyledSubtitle isMobile={isMobile}>
+                                    Monthly
+                                </StyledSubtitle>
+                                <Stack
+                                    direction="row"
+                                    justifyContent="space-between"
+                                >
+                                    <CustomizedBox
+                                        consumption={
+                                            // @ts-ignore
+                                            posts?.budget.month > 0
+                                                ? // @ts-ignore
+                                                  posts?.budget.month?.toFixed(
+                                                      3
+                                                  )
+                                                : posts?.budget.month
+                                        }
+                                        type="month"
+                                    />
+                                    <StyledText isMobile={isMobile}>
+                                        {posts?.budget.month} kgCO2eq{" "}
+                                    </StyledText>
+                                </Stack>
+                                <CustomizedMessage
+                                    // @ts-ignore
+                                    consumption={posts?.budget.month}
                                     type="month"
                                 />
-                                <StyledText isMobile={isMobile}>
-                                    {posts?.budget.month} kgCO2eq{" "}
-                                </StyledText>
                             </Stack>
-                            <CustomizedMessage
-                                // @ts-ignore
-                                consumption={posts?.budget.month}
-                                type="month"
-                            />
-                        </Stack>
 
-                        <Stack mb={2}>
-                            <StyledSubtitle isMobile={isMobile}>
-                                Annualy
-                            </StyledSubtitle>
-                            <Stack
-                                direction="row"
-                                justifyContent="space-between"
-                            >
-                                <CustomizedBox
+                            <Stack mb={2}>
+                                <StyledSubtitle isMobile={isMobile}>
+                                    Annualy
+                                </StyledSubtitle>
+                                <Stack
+                                    direction="row"
+                                    justifyContent="space-between"
+                                >
+                                    <CustomizedBox
+                                        // @ts-ignore
+                                        consumption={posts?.budget.year}
+                                        type="year"
+                                    />
+                                    <StyledText isMobile={isMobile}>
+                                        {/* @ts-ignore */}
+                                        {posts?.budget.year > 0
+                                            ? // @ts-ignore
+                                              posts?.budget.year?.toFixed(3)
+                                            : posts?.budget.year}{" "}
+                                        kgCO2eq{" "}
+                                    </StyledText>
+                                </Stack>
+                                <CustomizedMessage
                                     // @ts-ignore
                                     consumption={posts?.budget.year}
                                     type="year"
                                 />
-                                <StyledText isMobile={isMobile}>
-                                    {/* @ts-ignore */}
-                                    {posts?.budget.year > 0
-                                        ? // @ts-ignore
-                                          posts?.budget.year?.toFixed(3)
-                                        : posts?.budget.year}{" "}
-                                    kgCO2eq{" "}
-                                </StyledText>
                             </Stack>
-                            <CustomizedMessage
-                                // @ts-ignore
-                                consumption={posts?.budget.year}
-                                type="year"
-                            />
                         </Stack>
                     </Stack>
-                </Stack>
+                )}
             </StyledCard>
         </>
     );
